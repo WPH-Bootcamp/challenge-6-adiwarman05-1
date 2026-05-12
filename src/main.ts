c52d5fc31b9c06072317dd720c07218c9795a45c
adiwarman05 <adiwarman05@gmail.com>
2026-05-12 11:00:15 +0800

    add pengujian function di main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,17 @@ console.log("=====================================");
 
 // Mulai pengujian di bawah ini
 
+import { BookManager } from './functions/bookManager';
+
+const myApp = new BookManager();
+
+// 1. Contoh tambah beberapa data
+myApp.addBook("Laskar Pelangi", "Andrea Hirata", 2005);
+myApp.addBook("5 cm", "Donny Dhirgantoro", 2005);
+myApp.addBook("Ayat-Ayat Cinta", "Habiburrahman El Shirazy", 1994);
+
+// 2. Fitur Pencarian
+myApp.searchByTitle("Pelangi"); // Mencari buku yang mengandung kata "Pelangi"
+
+// 3. Fitur Filter
+myApp.filterByAuthor("Andrea Hirata"); // Menampilkan semua buku karya Andrea Hirata
